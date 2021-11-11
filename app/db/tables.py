303809be1493fcff52c270e19a72d8c60eca5c88
Dmitriy <dmitriyl1899@gmail.com>
2021-11-11 10:13:08 +0500

    Save date as string

--- app/db/tables.py
+++ app/db/tables.py
@@ -12,13 +12,11 @@ events_table = sa.Table(
     'events',
     metadata,
     sa.Column('id', sa.Integer, primary_key=True),
-    sa.Column('title', sa.String),
-    sa.Column('date_start', sa.Date, nullable=True),
-    sa.Column('date_end', sa.Date, nullable=True),
+    sa.Column('title', sa.String, nullable=False),
+    sa.Column('date', sa.String, nullable=True),
     sa.Column('short_description', sa.String, nullable=True),
     sa.Column('description', sa.String, nullable=True),
     sa.Column('place', sa.String, nullable=True),
     sa.Column('image_url', sa.String, nullable=True),
-    sa.Column('phone', sa.String, nullable=True),
     sa.Column('category_id', sa.Integer, sa.ForeignKey('event_categories.id'))
 )
